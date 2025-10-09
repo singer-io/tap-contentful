54288fe85cf7d0156d1e2ab56423ff3864abf231
mukeshbhatt18gl <mukesh.bhatt@qlik.com>
2025-10-09 17:29:47 +0530

    added schema files

--- tap_contentful/streams/environments.py
+++ tap_contentful/streams/environments.py
@@ -1,13 +1,19 @@
-from tap_contentful.streams.abstracts import ChildBaseStream
+from tap_contentful.streams.abstracts import ParentBaseStream
+from typing import Dict
 
-class Environments(ChildBaseStream):
+class Environments(ParentBaseStream):
     tap_stream_id = "environments"
     key_properties = ["sys.id"]
     replication_method = "INCREMENTAL"
     replication_keys = ["sys.updatedAt"]
     data_key = "items"
     path = "/spaces/{space_id}/environments"
-    parent = "spaces"
-    bookmark_value = None
     children = ["content_types", "entries", "assets", "locales", "tags", "tasks"]
 
+    def get_url_endpoint(self, parent_obj: Dict = None) -> str:
+        """
+        Get the URL endpoint for the stream
+        """
+        space_id = self.client.config.get("space_id")
+        formatted_path = self.path.format(space_id=space_id)
+        return self.url_endpoint or f"{self.client.base_url}{formatted_path}"
