2baf169358dcba2cb09e9a96a3fc431c561171ca
mukeshbhatt18gl <mukesh.bhatt@qlik.com>
2025-11-03 11:42:47 +0530

    dot notation sync changes

--- tap_contentful/streams/environments.py
+++ tap_contentful/streams/environments.py
@@ -17,3 +17,15 @@ class Environments(ParentBaseStream):
         space_id = self.client.config.get("space_id")
         formatted_path = self.path.format(space_id=space_id)
         return self.url_endpoint or f"{self.client.base_url}{formatted_path}"
+
+    def modify_object(self, record, parent_record=None):
+            """
+            Modify the record before writing to the stream.
+            Move sys.id -> id and sys.updatedAt -> updatedAt
+            """
+            sys_data = record.get("sys", {})
+
+            record["id"] = sys_data.get("id")
+            record["updatedAt"] = sys_data.get("updatedAt")
+
+            return record
