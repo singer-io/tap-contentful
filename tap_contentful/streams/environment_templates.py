0eddb6f2b003457d2e15e56bd16c30a73d142418
mukeshbhatt18gl <mukesh.bhatt@qlik.com>
2025-11-18 17:46:17 +0530

    update stream with parent id

--- tap_contentful/streams/environment_templates.py
+++ tap_contentful/streams/environment_templates.py
@@ -2,7 +2,7 @@ from tap_contentful.streams.abstracts import ChildBaseStream
 
 class EnvironmentTemplates(ChildBaseStream):
     tap_stream_id = "environment_templates"
-    key_properties = ["id"]
+    key_properties = ["id", "organization_id"]
     replication_method = "INCREMENTAL"
     replication_keys = ["updatedAt"]
     data_key = "items"
